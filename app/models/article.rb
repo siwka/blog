da0f4d0ee1d46d4591066bbb35787c5c15acf018
siwka <sivka@server.fake>
2012-12-19 00:52:38 -0600

    nauka

--- app/models/article.rb
+++ app/models/article.rb
@@ -6,6 +6,9 @@ class Article < ActiveRecord::Base
 	has_and_belongs_to_many :categories
 	has_many :comments
 
+	scope :published, where("articles.published_at IS NOT NULL")
+	scope :draft, where("articles.published_at IS NULL")
+
 	def long_title
 		"#{title} - #{published_at}"
 	end

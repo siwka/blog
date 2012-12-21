b03b33cc7dcf40bb9e879366dc012c2ab279e126
siwka <sivka@server.fake>
2012-12-20 23:01:10 -0600

    building validations

--- app/models/article.rb
+++ app/models/article.rb
@@ -8,9 +8,14 @@ class Article < ActiveRecord::Base
 
 	scope :published, where("articles.published_at IS NOT NULL")
 	scope :draft, where("articles.published_at IS NULL")
-	scope :recent, lambda{published.where("articles.published_at > ?", 1.week.ago.to_date)}
+	scope :recent, lambda {published.where("articles.published_at > ?", 1.week.ago.to_date)}
+	scope :where_title, lambda {|term| where("articles.title LIKE ?", "%#{term}%")}
 
 	def long_title
 		"#{title} - #{published_at}"
+	end
+
+	def published?
+		published_at.present?
 	end
 end

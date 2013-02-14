33b4178f73f82725273a52339076eaa87990277d
siwka <sivka@server.fake>
2013-02-13 23:05:40 -0600

    configured routes and resources

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,34 +1,40 @@
 class ArticlesController < ApplicationController
   # GET /articles
   # GET /articles.json
+  # GET /articles.xml
   def index
     @articles = Article.all
 
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @articles }
+      format.xml { render :xml => @articles }  # ruby ver 3.0 syntax?
     end
   end
 
   # GET /articles/1
   # GET /articles/1.json
+  # GET /articles/1.xml
   def show
     @article = Article.find(params[:id])
 
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @article }
+      format.xml { render :xml => @article}
     end
   end
 
   # GET /articles/new
   # GET /articles/new.json
+  # GET /articles/new.xml
   def new
     @article = Article.new
 
     respond_to do |format|
       format.html # new.html.erb
       format.json { render json: @article }
+      format.xml { render :xml => @article }
     end
   end
 
@@ -39,6 +45,7 @@ class ArticlesController < ApplicationController
 
   # POST /articles
   # POST /articles.json
+  # POST /articles.xml
   def create
     @article = Article.new(params[:article])
 
@@ -46,15 +53,18 @@ class ArticlesController < ApplicationController
       if @article.save
         format.html { redirect_to @article, notice: 'Article was successfully created.' }
         format.json { render json: @article, status: :created, location: @article }
+        format.xml { render :xml => @article, :status => :created, :location => @article }
       else
         format.html { render action: "new" }
         format.json { render json: @article.errors, status: :unprocessable_entity }
+        format.xml { render :xml => @article.errors, :status => :unprocessable_entity }
       end
     end
   end
 
   # PUT /articles/1
   # PUT /articles/1.json
+  # PUT /articles/1.xml
   def update
     @article = Article.find(params[:id])
 
@@ -62,15 +72,18 @@ class ArticlesController < ApplicationController
       if @article.update_attributes(params[:article])
         format.html { redirect_to @article, notice: 'Article was successfully updated.' }
         format.json { head :no_content }
+        format.xml { head :ok }
       else
         format.html { render action: "edit" }
         format.json { render json: @article.errors, status: :unprocessable_entity }
+        format.xml { render :xml => @article.errors, :status => :unprocessable_entity }
       end
     end
   end
 
   # DELETE /articles/1
   # DELETE /articles/1.json
+  # DELETE /articles/1.xml
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
@@ -78,6 +91,7 @@ class ArticlesController < ApplicationController
     respond_to do |format|
       format.html { redirect_to articles_url }
       format.json { head :no_content }
+      format.xml { head :ok }
     end
   end
 end
